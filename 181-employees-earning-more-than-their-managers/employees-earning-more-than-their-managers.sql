dfcc393c9f4aa94210509434ba94fe507fe24c86
Abdul Moiz <124524932+abdulmoiz248@users.noreply.github.com>
2024-05-26 11:08:16 +0500

    Time: 1029 ms (76.17%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,3 +1,3 @@
 /* Write your T-SQL query statement below */
-select name as Employee from Employee e
-where e.salary>(select salary from employee where id=e.managerId)
+select e.name as Employee from Employee e
+inner join Employee m on e.managerId=m.id where e.salary>m.salary
