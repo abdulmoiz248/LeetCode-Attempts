f34bf6e7fd6191fe5569cbae146c20e68af3052d
Abdul Moiz <124524932+abdulmoiz248@users.noreply.github.com>
2024-07-10 10:33:23 -0700

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         for(int i=0;i<nums.length;i++){
+            
             for(int j=i+1;j< nums.length;j++){
                 if(nums[i]==nums[j])
                     return true;
