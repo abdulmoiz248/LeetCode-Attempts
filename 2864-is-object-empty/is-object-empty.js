cfce8186557956f95b45f3a3c263d3f8b1cc4731
Abdul Moiz <124524932+abdulmoiz248@users.noreply.github.com>
2024-07-25 02:46:51 -0700

    Time: 52 ms (56.92%) | Memory: 49.1 MB (69.01%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,9 +3,5 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-   if(obj==null || obj==undefined) return true;
-
-   if(Array.isArray(obj)) return obj.length==0;
-
-  return  Object.keys(obj).length==0  
+   return _.isEmpty(obj)
 };
