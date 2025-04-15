e9cc24b7cdcd3687c2c7e9287d34ac010a651a41
Abdul Moiz <124524932+abdulmoiz248@users.noreply.github.com>
2025-04-15 21:40:00 +0500

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -3,6 +3,17 @@ import java.util.Map;
 import java.util.*;
 
 public class Solution {
+   public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int count=0;
+        for(int i=0;i<arr.length-2;i++)
+         for(int j=i+1;j<arr.length-1;j++)
+          for(int k=j+1;k<arr.length;k++){
+           if (Math.abs(arr[i] - arr[j]) <= a &&
+Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k])<= c) count++;
+          }
+
+        return count;
+    }
    public int maxScore(String s) {
         int ones=0;
         int ans=0;
