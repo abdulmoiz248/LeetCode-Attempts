ff738ee0d89ff8664a9c0994547612e8c9788dcf
Abdul Moiz <124524932+abdulmoiz248@users.noreply.github.com>
2025-04-23 14:39:47 +0500

    countLargestGroup

--- Solution.py
+++ Solution.py
@@ -14,3 +14,16 @@ class Solution(object):
             operation+=1
         
         return operation
+     def countLargestGroup(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        arr = {}
+        maximum = 0
+        for i in range(1, n + 1):
+            digitSum = sum(int(d) for d in str(i))
+            arr[digitSum] = arr.get(digitSum, 0) + 1
+            maximum = max(maximum, arr[digitSum])
+        
+        return sum(1 for count in arr.values() if count == maximum)
